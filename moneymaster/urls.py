5bde7a85493a21b2144c8a548a94dce8836eacba
fredmako <fredrickmakori102@gmail.com>
2024-05-03 15:30:32 +0300

    just added the functionalitie

--- moneymaster/urls.py
+++ moneymaster/urls.py
@@ -1,9 +1,12 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls import path, re_path
+from mymoney import views
+from mymoney.views import *
 
 urlpatterns = [
-  path('admin/', admin.site.urls),
-  path('mymoney/', include('mymoney.urls', namespace='mymoney')),  # Include mymoney app's URLs with namespace prefix
-  # ... other URL patterns for your project
+    path('admin/', admin.site.urls),
+    re_path('^get_chain$', views.get_chain, name="get_chain"),
+    re_path('^mine_block$', views.mine_block, name="mine_block"),
+    re_path('^is_valid$', views.is_valid, name="is_valid"),
 ]
